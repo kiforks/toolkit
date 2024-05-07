4b7dc94b04ffdc96c04d69b969270ed05b204c0e
kiforks <ks.kifor@gmail.com>
2024-05-07 21:56:34 +0300

    fix: changed app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { RouterModule } from '@angular/router';
 	changeDetection: ChangeDetectionStrategy.OnPush,
 	standalone: true,
 	imports: [RouterModule],
-	selector: 'app-root',
+	selector: '[appRoot]',
 	templateUrl: './app.component.html',
 })
 export class AppComponent {
