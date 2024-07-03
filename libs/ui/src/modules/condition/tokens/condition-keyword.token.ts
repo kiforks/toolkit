02e176d59e6ff85c96b73b513c817ef05b9148e0
Kostiantyn Kifor <ks.kifor@gmail.com>
2024-07-03 17:26:22 +0300

    feat: add new condition module

---
+++ libs/ui/src/modules/condition/tokens/condition-keyword.token.ts
@@ -0,0 +1,5 @@
+import { InjectionToken } from '@angular/core';
+
+import { ConditionKeyword } from '../interfaces';
+
+export const CONDITION_KEYWORD = new InjectionToken<ConditionKeyword>('CONDITION_KEYWORD');
