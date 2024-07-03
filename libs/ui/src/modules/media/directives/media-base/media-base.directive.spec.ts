7ecdb7e6409adfb483f8aa34d339526cadb715d8
Kostiantyn Kifor <ks.kifor@gmail.com>
2024-07-03 20:52:34 +0300

    refactor: change tests

--- libs/ui/src/modules/media/directives/media-base/media-base.directive.spec.ts
+++ libs/ui/src/modules/media/directives/media-base/media-base.directive.spec.ts
@@ -1,16 +1,12 @@
-import { createDirectiveFactory, SpectatorDirective } from '@ngneat/spectator/jest';
+import { createDirectiveFactory } from '@ngneat/spectator/jest';
 
 import { MediaBaseDirective } from './media-base.directive';
-import { MediaBaseDirectivePO } from './media-base.directive.po';
 
 import { MediaElementMock } from '../../mocks/media-element.mock';
 
 import { MEDIA_ELEMENT } from '../../tokens';
 
 describe('MediaBaseDirective', () => {
-	let spectator: SpectatorDirective<MediaBaseDirective>;
-	let directivePO: MediaBaseDirectivePO;
-
 	const createDirective = createDirectiveFactory(MediaBaseDirective);
 
 	it('should dynamically render directive content', () => {
@@ -18,7 +14,7 @@ describe('MediaBaseDirective', () => {
 
 		const spyOnCheckMedia = jest.spyOn(mediaElementMock, 'checkMedia');
 
-		spectator = createDirective(
+		createDirective(
 			`
 				<div
 					*ksMediaBase
@@ -30,25 +26,18 @@ describe('MediaBaseDirective', () => {
 			{ providers: [{ provide: MEDIA_ELEMENT, useValue: mediaElementMock }] }
 		);
 
-		directivePO = new MediaBaseDirectivePO(spectator);
-
-		expect(directivePO.element).toExist();
-		expect(directivePO.elements).toHaveLength(1);
+		expect(mediaElementMock.condition()).toBe(true);
 
 		expect(spyOnCheckMedia).toHaveBeenNthCalledWith(1, mediaElementMock.breakpoint());
 
 		mediaElementMock.setCheckMedia(false);
-		spectator.detectChanges();
 
-		expect(directivePO.element).not.toExist();
-		expect(directivePO.elements).toHaveLength(0);
+		expect(mediaElementMock.condition()).toBe(false);
 		expect(spyOnCheckMedia).toHaveBeenNthCalledWith(1, mediaElementMock.breakpoint());
 
 		mediaElementMock.setCheckMedia(true);
-		spectator.detectChanges();
 
-		expect(directivePO.element).toExist();
-		expect(directivePO.elements).toHaveLength(1);
+		expect(mediaElementMock.condition()).toBe(true);
 		expect(spyOnCheckMedia).toHaveBeenNthCalledWith(1, mediaElementMock.breakpoint());
 	});
 });
